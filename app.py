2bf88f4e8c7cbe7dc7a975d03a3962d3478c1208
Omonigho4 <109107792+Omonigho4@users.noreply.github.com>
2024-02-11 06:39:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    cpu_percent = psutil.cpu_percent()
-    mem_percent = psutil.virtual_memory().percent
-    Message = None
-    if cpu_percent > 80 or mem_percent > 80:
-        Message = "High CPU or memory  detected, Please scale up!!!!"
-    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent, message=Message)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    def hello_world():
+        return "<p>Hello, World!</p>"
+  
+if __name__ == "__main__" :
+    welcome.run(host="0.0.0.0", port = int("5000") , debug=True )
