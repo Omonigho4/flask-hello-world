1c851cede1f10e2ccbc5423748b0ef6e9ae4f5ad
Omonigho4 <109107792+Omonigho4@users.noreply.github.com>
2024-02-11 07:12:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    def hello_world():
+def hello_world():
         return "<p>Hello, World!</p>"
   
 if __name__ == "__main__" :
