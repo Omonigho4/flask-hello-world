f4e505dd390931eadc311e3463ae9b3153362b6b
Omonigho4 <109107792+Omonigho4@users.noreply.github.com>
2024-02-11 07:26:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import psutil
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-        return "<p>Hello, World!</p>"
-
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__" :
-    welcome.run(host="0.0.0.0", port = int("5000") , debug=True )
+    app.run(host="0.0.0.0", port = int("5000") , debug=True )
